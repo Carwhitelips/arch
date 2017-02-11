f4e941b3c5d7a687b231df756d0ee8b19eb4712d
Darwin Cahyadi <dwncy@Darwins-MacBook-Pro.local>
2017-02-11 20:54:15 +0700

    Update base style
    Summary:
    ignore arch.css
    
    remove unused line
    
    Test Plan: manual
    
    Reviewers: pohon
    
    Reviewed By: pohon
    
    Differential Revision: https://carwhitelips.phacility.com/D5

--- header.php
+++ header.php
@@ -5,17 +5,21 @@
  * Displays all of the <head> section and everything up till </header>
  *
  * @package _arch
+ * @author: darwin <darwin301194@gmail.com>
  */
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
   <head>
     <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php wp_title('|','true','right'); ?><?php bloginfo('name'); ?></title>
     <link href="<?php bloginfo('template_directory');?>/arch.css" rel="stylesheet">
     <?php wp_head(); ?>
   </head>
   <body <?php body_class(); ?>>
     <header>
-      <a href="#">Arch.</a>
+      <nav class="site-nav">
+        <a href="#" class="site-nav__home-link">Arch.</a>
+      </nav>
     </header>
